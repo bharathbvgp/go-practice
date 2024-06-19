f8bdf65919a6e2c2fcfe1bb489b995356bac61cf
Bharath <bharath@Balrajs-MacBook-Pro-2.local>
2024-06-19 11:03:09 +0530

    learnt about struct embedding

---
+++ structembedding/struct.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+type container struct {
+	// EMBEDDING A STRUCT INSIDE OF STRUCT
+	base 
+	str string
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num = %v" , b.num);
+}
+
+func main() {
+
+	co := container {
+		base : base {
+			num : 10,
+		},
+		str : "bharath",
+	}
+
+	fmt.Println(co.num);
+	fmt.Println(co.base.num); // accessing via full path
+	fmt.Println(co.describe());
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co;
+	fmt.Println(d.describe());
+
+}
